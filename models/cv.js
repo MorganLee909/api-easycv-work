1ec2837305cca649906f6b274e516050c026a063
Lee Morgan <me@leemorgan.io>
2023-01-01 16:55:53 -0500

    Prevent creation of collection for subdocument.

--- models/cv.js
+++ models/cv.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const EmploymentSchema = new mongoose.Schema({
+    autoCreate: false,
+    autoIndex: false,
     position: {
         type: String,
         required: true
