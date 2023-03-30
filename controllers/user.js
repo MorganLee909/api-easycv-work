503668afa7058dd14a413dbf6e7d4d565fae1e0c
Lee Morgan <me@leemorgan.io>
2023-03-30 12:28:35 -0400

    Creating new user creates empty array for socials.

--- controllers/user.js
+++ controllers/user.js
@@ -84,6 +84,7 @@ module.exports = {
                     jobCategory: "",
                     experience: 0,
                     skills: [],
+                    socials: [],
                     workHistory: []
                 });
 
